594d6c80af472f0e44538d7b4bf718e5deb758b1
nurulintan09 <0110124122@student.nurulfikri.ac.id>
2025-05-08 16:30:30 +0700

    tugas 8

---
+++ pertemuan 7/my-app/app/Http/Controllers/UnitKerjaController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\UnitKerja;
+use Illuminate\Http\Request;
+
+class UnitKerjaController extends Controller
+{
+    public function index()
+    {
+        $unitkerja = UnitKerja::all();
+
+        return view("unit-kerja.index", compact("unitkerja"));
+    }
+}
